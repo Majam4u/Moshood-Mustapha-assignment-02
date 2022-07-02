700aead0f09cb2824297a098f08be82d7e3fdff4
Majam4u <49521798+Majam4u@users.noreply.github.com>
2022-07-02 11:03:04 -0500

    myJS Assesment Solution

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,15 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+
+    let sum = 0;
+
+    for (let i of arrayOfNumbers) {
+        sum += i;
+    } return sum;
+    // return arrayOfNumbers.length;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +34,17 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+
+    let counter = 0;
+
+    for (let i of arrayOfNumbers) {
+        if(i % 2 === 0)
+        counter += 1;
+    } return counter;
+
+    // return arrayOfNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
